f941402a35b213f32784d82692e314416b693548
Consultant <consultant@Admins-MBP-3.lan>
2023-09-24 16:56:26 -0400

    Initial commit

---
+++ src/main/kotlin/MyWolf.kt
@@ -0,0 +1,11 @@
+class MyWolf {
+    var wolf: Wolf? = Wolf()
+
+    fun myFunction(){
+        wolf?.eat()
+    }
+}
+
+fun getAlphaWolf(): Wolf? {
+    return Wolf()
+}
---
+++ src/main/kotlin/Wolf.kt
@@ -0,0 +1,8 @@
+class Wolf {
+    var hunger = 10
+    val food = "meat"
+
+    fun eat(){
+        println("The wolf is eating $food.")
+    }
+}
